636d81583789c8ea38adb1d9bcccef8415f8bdfb
gudivadaashok <gudivadaashok@users.noreply.github.com>
2025-10-29 00:32:18 -0400

    fix: Update RViz launch script to run as ubuntu user with X11 auth
    - Run RViz as ubuntu user to match X11 permissions
    - Add XAUTHORITY export for proper X11 authentication
    - Add sleep to wait for X server initialization

--- scripts/launch-rviz.sh
+++ scripts/launch-rviz.sh
@@ -8,8 +8,12 @@ source /opt/ros/humble/setup.bash
 source /tmp/ros2_deps/install/setup.bash
 source /home/ubuntu/ros2_ws/install/setup.bash
 
-# Set display
+# Set display and X11 auth
 export DISPLAY=:1
+export XAUTHORITY=/home/ubuntu/.Xauthority
 
-# Launch RViz2 with config
-rviz2 -d /usr/local/share/musohu_sensors.rviz
+# Wait a moment for X server
+sleep 2
+
+# Launch RViz2 with config as ubuntu user
+su - ubuntu -c "export DISPLAY=:1 && source /opt/ros/humble/setup.bash && source /home/ubuntu/ros2_ws/install/setup.bash && rviz2 -d /usr/local/share/musohu_sensors.rviz"
